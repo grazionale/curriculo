d5a1ad084d932d0a37cde124576ae1a22644f9fb
Gabriel Grazionale <gabriel.grazionale@pado.com.br>
2019-04-11 13:00:38 -0300

    Requisição a API Mockable.io para obter a experiencia profissional

--- dist_portifolio/js/scripts.js
+++ dist_portifolio/js/scripts.js
@@ -20,3 +20,32 @@ function close_menu(){
     document.getElementsByTagName('header')[0].classList.remove("active");
     document.getElementsByTagName('body')[0].classList.remove("active");
 }
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://demo8378319.mockable.io/empresas');
+xhr.onload = function() {
+    if (xhr.status === 200) {
+        data = JSON.parse(xhr.response);
+        for(i = 0 ; i < data.length ; i++){
+            document.getElementById("experience").innerHTML += 
+            '<div class="row">'+
+                '<div class="col-md-12">'+
+                    '<div id="experience-timeline">'+
+                        '<div class="vtimeline-content">'+
+                        '<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div><h3>' + data[i]["empresa"] + '</h3>' +
+                        '<h4>' + data[i]["cargo"] + '</h4>' +
+                        '<p>' +
+                            data[i]['atividades']
+                        '</p>' +
+                        '</div>'+
+                    '</div>' +
+                '</div>' +
+            '</div>';
+        }
+    }
+    else {
+        console.log('A requisição falhou ' + xhr.status);
+    }
+};
+xhr.send();
+
